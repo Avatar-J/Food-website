9b7cc53ab3edbb536653ee78e7e8c71960fd3339
Jummai <jummyhassan5@gmail.com>
2023-07-29 16:46:47 +0000

    added a data file

--- zafa-foods/src/Pages/AboutUs/MeetOurChefs/MeetOurChefs.tsx
+++ zafa-foods/src/Pages/AboutUs/MeetOurChefs/MeetOurChefs.tsx
@@ -1,44 +1,14 @@
 import React from "react";
-import Derrick from "../../../Images/derrick.jpg";
-import Amidu from "../../../Images/Amidu.jpg";
-import John from "../../../Images/John.jpg";
-import Afia from "../../../Images/afia.jpg";
-import Hamza from "../../../Images/Hamza.jpg";
-import Akosua from "../../../Images/Akosua.jpg";
-import "./MeetOurChefs.css";
+import { chefData } from "../../../Data";
 import {
   SectionHeader,
   SectionContainer,
   CardSectionWrapper,
   Card,
-  CardImg,
   CardTitle,
   ImageAdjust,
   CardOverlay,
 } from "../../../Components/GeneralStyling";
-
-const chefs = [
-  {
-    name: "Chef Derrick",
-    img: Derrick,
-    info: "Meet Chef Derrick Smith, a culinary virtuoso with a passion for French cuisine, creating dishes that leave a lasting impression with delightful fusion of classic techniques and contemporary twists",
-  },
-  {
-    name: "Chef Amidu",
-    img: Hamza,
-    info: "Discover the culinary brilliance of Chef Amidu Alhassan, whose passion for farm-to-table cooking shines through in every dish, offering a delightful symphony of flavors that celebrate the freshest seasonal ingredients",
-  },
-  {
-    name: "Chef Mary",
-    img: Akosua,
-    info: "Introducing Chef Akosua Agyapong, an award-winning culinary artist renowned for her exquisite pastry creations, infusing flavors from around the world into delightful desserts that captivate every diner's palate",
-  },
-  {
-    name: "Chef Afia",
-    img: Afia,
-    info: "Introducing Chef Afia Danso, a rising star in the culinary world, known for her artistic presentations and a mastery of bold flavors that awaken the senses, offering a dining experience that is as visually stunning as it is delicious",
-  },
-];
 
 function MeetOurChefs() {
   return (
@@ -47,7 +17,7 @@ function MeetOurChefs() {
         <SectionHeader>Meet Our Chefs</SectionHeader>
 
         <CardSectionWrapper>
-          {chefs.map((chef) => {
+          {chefData.map((chef) => {
             return (
               <Card>
                 <ImageAdjust src={chef.img} alt={chef.name} />
@@ -66,11 +36,3 @@ function MeetOurChefs() {
 }
 
 export default MeetOurChefs;
-
-{
-  /* 
-                
-
-                
-              </div> */
-}
--- zafa-foods/src/Pages/Branches/Branches.tsx
+++ zafa-foods/src/Pages/Branches/Branches.tsx
@@ -10,56 +10,7 @@ import {
   ImageAdjust,
   CardOverlay,
 } from "../../Components/GeneralStyling";
-import Madina from "../../Images/branch1.jpg";
-import Ridge from "../../Images/branch2 (1).jpg";
-import branch3 from "../../Images/branch2 (2).jpg";
-import branch4 from "../../Images/branch2 (3).jpg";
-import MD_exterior from "../../Images/MD_exteriorView.jpg";
-import RD_exterior from "../../Images/RD_exteriorView.jpg";
-import MD_employee from "../../Images/MD_employee_of_the_year.jpg";
-import RD_employee from "../../Images/RD_employee_of_the_year.jpg";
-import MD_customers from "../../Images/customers.jpg";
-
-const branchDetails = [
-  {
-    name: "Madina Branch",
-    paragraph:
-      "Step into our exquisite restaurant branch and indulge in a culinary experience like no other, as our chef's special dishes offer mouthwatering and flavorful delights that are both savory and delectable",
-    img: Madina,
-
-    CardItems: [
-      {
-        img: MD_exterior,
-        title: "exterior view",
-      },
-      {
-        img: MD_employee,
-        title: "employee of the year",
-      },
-      {
-        img: MD_customers,
-        title: "Customers",
-      },
-    ],
-  },
-  {
-    name: "Ridge Branch",
-    paragraph:
-      "Experience the warm hospitality and vibrant atmosphere of our authentic restaurant branch, where you can savor the luscious and unforgettable flavors of our handcrafted gourmet dishes.",
-    img: Ridge,
-
-    CardItems: [
-      {
-        img: RD_exterior,
-        title: "exterior view",
-      },
-      {
-        img: RD_employee,
-        title: "employee of the year",
-      },
-    ],
-  },
-];
+import { branchDetails } from "../../Data";
 
 function Branches() {
   return (
