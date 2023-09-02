bb4eac44055bee403a57c47d84855a21dbcdf9b3
Jummai <jummyhassan5@gmail.com>
2023-09-02 12:07:29 +0000

    animation to image slider

--- zafa-foods/src/Components/GeneralStyling.js
+++ zafa-foods/src/Components/GeneralStyling.js
@@ -119,6 +119,7 @@ export const ImageAdjust = styled.img`
   width: 100%;
   height: 100%;
   object-fit: cover;
+  object-position: center;
 `;
 
 export const CardOverlay = styled.div`
--- zafa-foods/src/Pages/HomePage/Branches.tsx
+++ zafa-foods/src/Pages/HomePage/Branches.tsx
@@ -1,9 +1,20 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { SectionHeader, ImageAdjust } from "../../Components/GeneralStyling";
 import { branchDetails } from "../../Data";
 import styled from "styled-components";
 
 function Branches() {
+  const [currentImg, setCurrentImg] = useState(0);
+
+  const nextImg = () => {
+    setCurrentImg((prevIndex) => (prevIndex + 1) % 3);
+  };
+
+  useEffect(() => {
+    const interval = setInterval(nextImg, 3000);
+
+    return () => clearInterval(interval);
+  }, []);
   return (
     <>
       <SectionHeader>Branches</SectionHeader>
@@ -18,7 +29,17 @@ function Branches() {
               </div>
 
               <div className="img-container">
-                {item.img.map((img) => {
+                <div className="branch-img-container">
+                  <ImageAdjust
+                    src={item.img[currentImg]}
+                    alt={item.name}
+                    style={{
+                      clipPath: "circle(0% at 0% 100%)",
+                      transition: "all 0.7s",
+                    }}
+                  />
+                </div>
+                {/* {item.img.map((img) => {
                   return (
                     <>
                       <div className="branch-img-container">
@@ -26,7 +47,7 @@ function Branches() {
                       </div>
                     </>
                   );
-                })}
+                })} */}
               </div>
             </BranchSection>
           </>
@@ -53,12 +74,17 @@ const BranchSection = styled.div<BranchSectionProps>`
     width: 50%;
     height: 100%;
     /* background-color: rgba(0, 0, 0, 0.5); */
-    display: flex;
+    display: grid;
+    grid-auto-flow: column;
+    overflow: hidden;
   }
 
   & .branch-img-container {
     width: 100%;
     height: 100%;
+    /* opacity: 1;
+    transition: 5s ease-in-out; */
+    /* animation: slide-show 8s infinite; */
   }
 
   & .branch-description {
@@ -81,6 +107,25 @@ const BranchSection = styled.div<BranchSectionProps>`
     border-radius: 50%;
     z-index: -1;
   }
+  @keyframes slide-show {
+    0% {
+      opacity: 0.3;
+    }
+    10% {
+      opacity: 1;
+    }
+    20%,
+    30% {
+      opacity: 1;
+      scale: 1.03;
+    }
+  }
+
+  /* @keyframes slide-show1 {
+    0%{
+      translate: ;
+    }
+  } */
 
   /*For mobile phones*/
   @media only screen and (max-width: 768px) {
